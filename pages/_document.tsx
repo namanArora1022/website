9a146803253b441f9dd30d36bd75e273e613d4d0
Naman Arora <namanarora1022@gmail.com>
2022-10-13 23:03:18 +0530

    added meta tags

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <meta
+                    name="description"
+                    content="I can develop both the front-end and back-end of a web application. I am comfortable working with a wide range of technologies and am always willing to learn new ones.Here is a precise description of my awesome webpage."
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
